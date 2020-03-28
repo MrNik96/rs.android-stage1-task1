9e175d509dbaa43393e0d18d3fdfc6f51460b3ac
mrnik96 <nekalo96@gmail.com>
2020-03-28 11:33:29 +0300

    create MinMax

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -2,8 +2,17 @@ package subtask2
 
 class MiniMaxSum {
 
-    // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        input.sort()
+        return intArrayOf(minSum(input), maxSum(input))
+    }
+
+    private fun minSum(array: IntArray) : Int{
+        return array.sum() - array.last()
+    }
+
+    private fun maxSum(array: IntArray) : Int{
+        val reversedArray = array.reversedArray()
+        return reversedArray.sum() - reversedArray.last()
     }
 }
