e60c91ce1e5c54c40b1b935db1ef3e35cdb7229a
mrnik96 <nekalo96@gmail.com>
2020-03-29 10:30:29 +0300

    create HighestPalindrome

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -4,6 +4,21 @@ class HighestPalindrome {
 
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        val charArray = digitString.toCharArray()
+        var count = 0
+        for (index in charArray.indices) {
+            if (index > charArray.size / 2) break
+
+            if (charArray[index] != charArray[charArray.size - 1 - index]) {
+                if (charArray[index] == '0') {
+                    charArray[index] = '9'
+                }
+                charArray[charArray.size - 1 - index] = charArray[index]
+                count++
+            } else if (count > k) {
+                return "-1"
+            }
+        }
+        return String(charArray)
     }
 }
